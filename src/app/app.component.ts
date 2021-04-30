69ef2f8b63206f763228c73469adaabff75b1ac3
aviyot <aviyot@gmail.com>
2021-05-01 00:51:49 +0300

    arrange auth comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
@@ -9,15 +9,27 @@ import firebase from 'firebase/app';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Start Template : Angular,AngularFire,AngularMaterial';
-  appName = "my app"
+  appName = "my app";
+  isUserSignIn = false;
   items: Observable<any[]>;
+  isReg = false;
   constructor(private firestore: AngularFirestore,public auth: AngularFireAuth) {
     this.items = firestore.collection('items').valueChanges();
   
   }
-
+   ngOnInit(){
+     this.auth.user.subscribe((user)=>{
+       if(user){
+        this.isUserSignIn = true;
+       }
+       else {
+        this.isUserSignIn = false;
+       }
+      
+     })
+   }
   addItem() {
       this.firestore.collection("items").add({
       name: "new york",
@@ -34,5 +46,13 @@ export class AppComponent {
   }
   logout() {
     this.auth.signOut();
+  }
+
+  register(){
+    this.isReg = true;
+  }
+
+  loginReq(){
+    this.isReg = false;
   }
 }
