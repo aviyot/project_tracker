7501712af6d92e59b7febcbc431084b9f1ba8278
aviyot <aviyot@gmail.com>
2021-05-01 23:57:53 +0300

    change app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  appName = 'my app';
+  appName = 'Project tracker';
   isUserSignIn = false;
   collectionName: string = 'items';
   items: Observable<any[]>;
