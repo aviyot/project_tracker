7749c1b70616864e16f08623fc36499ff7dbc5b3
aviyot <aviyot@gmail.com>
2021-05-20 20:36:03 +0300

    chage interface

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -2,8 +2,10 @@ import { tool } from './tool.model';
 import { Feature } from './feature.model';
 import {QuestionAnswer} from "./question-answer.model"
 import { LIFECYCLE_STAGE } from '../types/lifecycleStage.type';
+import { ProjectData } from './project-data.model';
 
-export interface Project {
+export interface Project extends ProjectData {
+  id:string;
   name: string;
   desc: string;
   startTime: string;
