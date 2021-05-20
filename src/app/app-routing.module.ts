e3f66fcac86f823effbfa9d95efc0615285db7ce
aviyot <aviyot@gmail.com>
2021-05-20 20:29:54 +0300

    add two routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { NewProjectComponent } from './forms/new-project/new-project.component';
+import { ProjectComponent } from './pages/project/project.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
-  {path:'new-project',component:NewProjectComponent}
+  {path:'new-project',component:NewProjectComponent},
+  {path:'new-project/edit/:project_id',component:NewProjectComponent},
+  {path:'app-project/:project_id',component:ProjectComponent}
 ];
 
 @NgModule({
