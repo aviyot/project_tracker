8674cb50c094591718b4fea23fb6ae082960d6ed
aviyot <aviyot@gmail.com>
2021-06-16 22:48:48 +0300

    remove the old form to enter new project

--- src/app/forms/new-project/new-project.component.ts
+++ src/app/forms/new-project/new-project.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ProjectData } from 'src/models/project-data.model';
-import { WorkTime } from 'src/models/work-time.model';
-import { HowTodo } from '../../../../src/models/howtodo.model';
 
 @Component({
   selector: 'app-new-project',
@@ -15,331 +12,44 @@ import { HowTodo } from '../../../../src/models/howtodo.model';
 export class NewProjectComponent implements OnInit {
   projects: ProjectData[] = [];
   currentProject: ProjectData;
-  doc_id: string;
   user: any;
   new: boolean = true;
-  projectForm: FormGroup;
+  added = false;
+  exit = false;
 
   constructor(
-    private fb: FormBuilder,
     private firestore: AngularFirestore,
     private auth: AngularFireAuth,
-    private activatedRoute: ActivatedRoute,
     private router: Router
-  ) {
-    this.createform();
-  }
+  ) {}
   ngOnInit(): void {
     this.auth.user.subscribe((user) => {
       if (user) {
         this.user = user;
+      }
+    });
+  }
 
-        this.activatedRoute.paramMap.subscribe((p) => {
-          this.doc_id = p.get('project_id');
-          if (this.doc_id) {
-            this.new = false;
-            this.firestore
-              .collection('users')
-              .doc(user.uid)
-              .collection('projects')
-              .doc(p.get('project_id'))
-              .get()
-              .subscribe((p) => {
-                const { features, questions, todos, tools, howTodos,workTimes} =
-                  p.data();
-                this.projectForm.patchValue(p.data());
-                this.addFeature(features);
+  updateNewProject(newProject: ProjectData) {
+    this.currentProject = newProject;
+  }
 
-                this.addQuestion(questions);
-                questions.forEach((el, index) => {
-                  this.addAnswerLink(index, el.answerLinks);
-                });
-
-                this.addTool(tools);
-
-                this.addTodo(todos);
-               /*  this.addHowTodo(howTodos);
-                howTodos.forEach((el, index) => {
-                  this.addHowtodoSteps(index, el.steps);
-                }); */
-                this.addWorkTime(workTimes);
-              });
+  saveFormData(exit?: boolean) {
+    if (this.user.uid) {
+      this.firestore
+        .collection('users')
+        .doc(this.user.uid)
+        .collection('projects')
+        .add(this.currentProject)
+        .then((doc) => {
+          console.log(doc.id);
+          this.added = true;
+          if (exit) {
+            this.router.navigate(['./', 'app-project', doc.id]);
           }
         });
-      }
-    });
-  }
-
-  createform() {
-    this.projectForm = this.fb.group({
-      name: ['', [Validators.required]],
-      desc: [''],
-      startTime: [''],
-      endTime: [''],
-      lifecycleStage: [''],
-      gitHub: [''],
-      site:[''],
-      tools: this.fb.array([]),
-      todos: this.fb.array([]),
-      features: this.fb.array([]),
-      questions: this.fb.array([]),
-      // howTodos: this.fb.array([]),
-      workTimes:this.fb.array([])
-    });
-  }
-
-  saveFormData(exit?:boolean) {
-    this.currentProject = { ...this.projectForm.value };
-    if (this.new) {
-      if (this.user.uid) {
-        if (this.projectForm.valid) {
-          this.firestore
-            .collection('users')
-            .doc(this.user.uid)
-            .collection('projects')
-            .add(this.currentProject)
-            .then(() => {
-              this.projectForm.reset();
-            });
-        } else {
-          console.log('form not vaild');
-        }
-      } else {
-        console.log('invaild user data');
-      }
     } else {
-      if (this.user.uid) {
-        if (this.projectForm.valid) {
-          this.firestore
-            .collection('users')
-            .doc(this.user.uid)
-            .collection('projects')
-            .doc(this.doc_id)
-            .update(this.currentProject)
-            .then(() => {
-              alert("data saved");
-              if(exit){
-              this.projectForm.reset();
-              this.router.navigate(['/', 'app-project', this.doc_id]);
-              }
-            });
-        } else {
-          console.log('form not vaild');
-        }
-      } else {
-        console.log('invaild user data');
-      }
-    }
-  }
-
-  addTodo(data?: any[]) {
-    const formArray = this.projectForm.get('todos') as FormArray;
-    const todo = this.fb.group({
-      title: [''],
-      detail: [''],
-      date: [''],
-      done: [false],
-    });
-    if (data) {
-      data.forEach((dataItem) => {
-        formArray.push(
-          this.fb.group({
-            title: [dataItem.title],
-            detail: [dataItem.detail],
-            date: [dataItem.date],
-            done: [dataItem.done],
-          })
-        );
-      });
-    } else {
-      formArray.push(todo);
-    }
-  }
-
-  addTool(data?: any[]) {
-    const formArray = this.projectForm.get('tools') as FormArray;
-    const tool = this.fb.group({
-      name: [''],
-      desc: [''],
-      purpose: [''],
-      ver: [''],
-      webSite: [''],
-      githubLink: [''],
-      npmLink: [''],
-    });
-    if (data) {
-      data.forEach((dataItem) => {
-        formArray.push(
-          this.fb.group({
-            name: [dataItem.name],
-            desc: [dataItem.desc],
-            purpose: [dataItem.purpose],
-            ver: [dataItem.ver],
-            webSite: [dataItem.webSite],
-            githubLink: [dataItem.githubLink],
-            npmLink: [dataItem.npmLink],
-          })
-        );
-      });
-    } else {
-      formArray.push(tool);
-    }
-  }
-
-  addFeature(data?: any[]) {
-    const formArray = this.projectForm.get('features') as FormArray;
-    const feature = this.fb.group({
-      name: [''],
-      desc: [''],
-      startDate: [''],
-      endDate: [''],
-      progress: [''],
-    });
-    if (data) {
-      data.forEach((dataItem) => {
-        formArray.push(
-          this.fb.group({
-            name: [dataItem.name],
-            desc: [dataItem.desc],
-            startDate: [dataItem.startDate],
-            endDate: [dataItem.endTime],
-            progress: [dataItem.progress],
-          })
-        );
-      });
-    } else {
-      formArray.push(feature);
-    }
-  }
-
-  addQuestion(data?: any[]) {
-    const formArray = this.projectForm.get('questions') as FormArray;
-    const question = this.fb.group({
-      question: [''],
-      questionTime: [''],
-      lifecycleStageId: [''],
-      featureId: [''],
-      answered: [''],
-      answer: [''],
-      answerTime: [''],
-      answerLinks: this.fb.array([]),
-    });
-    if (data) {
-      data.forEach((dataItem) => {
-        formArray.push(
-          this.fb.group({
-            question: [dataItem.question],
-            questionTime: [dataItem.questionTime],
-            lifecycleStageId: [dataItem.lifecycleStageId],
-            featureId: [dataItem.featureId],
-            answered: [dataItem.answered],
-            answer: [dataItem.answer],
-            answerTime: [dataItem.answerTime],
-            answerLinks: this.fb.array([]),
-          })
-        );
-      });
-    } else {
-      formArray.push(question);
-    }
-  }
-
-  addAnswerLink(index, data?: any[]) {
-    const answerLinkRef = this.getAnswerLinks(index);
-    const answerLink = this.fb.group({
-      from: [''],
-      link: [''],
-      prefer: [''],
-    });
-
-    if (data) {
-      data.forEach((dataItem) => {
-        answerLinkRef.push(
-          this.fb.group({
-            from: [dataItem.from],
-            link: [dataItem.link],
-            prefer: [dataItem.prefer],
-          })
-        );
-      });
-    } else {
-      answerLinkRef.push(answerLink);
-    }
-  }
-/* 
-  addHowTodo(data?: HowTodo[]) {
-    const formArray = this.projectForm.get('howTodos') as FormArray;
-    const howTodo = this.fb.group({
-      title: [''],
-      desc: [''],
-      ordered: [false],
-      steps: this.fb.array([]),
-    });
-
-    if (data) {
-      data.forEach((dataItem) => {
-        formArray.push(
-          this.fb.group({
-            title: [dataItem.title],
-            desc: [dataItem.desc],
-            ordered: [dataItem.ordered],
-            steps: this.fb.array([]),
-          })
-        );
-      });
-    } else {
-      formArray.push(howTodo);
-    }
-  }
-
-  addHowtodoSteps(index: number, data?: string[]) {
-    const todoStepsRef = this.getHowtodoSteps(index);
-    const todoStep = this.fb.control('');
-    if (data) {
-      data.forEach((dataItem) => {
-        todoStepsRef.push(this.fb.control(dataItem));
-      });
-    } else {
-      todoStepsRef.push(todoStep);
-    }
-  }
-  getHowtodoSteps(index): FormArray {
-   console.log((this.projectForm.get('howTodos') as FormArray).at(index));
-    return (this.projectForm.get('howTodos') as FormArray)
-      .at(index)
-      .get('steps') as FormArray;
-  }
- */
-  getAnswerLinks(index): FormArray {
-    return (this.projectForm.get('questions') as FormArray)
-      .at(index)
-      .get('answerLinks') as FormArray;
-  }
-
-  removeAnswerLink(questionIndex, answerLinkIndex) {
-    this.getAnswerLinks(questionIndex).removeAt(answerLinkIndex);
-  }
-
-  addWorkTime(data?:WorkTime[]){
-    const formArray = this.projectForm.get('workTimes') as FormArray;
-    const workTime = this.fb.group({
-      startTime:[''],
-      endTime:[''],
-      task:['']
-    })
-
-    if(data) {
-      data.forEach((dataItem)=>{
-        formArray.push(this.fb.group({
-          startTime:[dataItem.startTime],
-          endTime:[dataItem.endTime],
-          task:[dataItem.task]
-        }))
-
-      })
-    }
-    else {
-      formArray.push(workTime);
+      console.log('invaild user data');
     }
   }
 }
--- src/app/forms/project-desc-form/project-desc-form.component.ts
+++ src/app/forms/project-desc-form/project-desc-form.component.ts
@@ -1,33 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { AngularFirestoreDocument } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProjectData } from 'src/models/project-data.model';
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-project-desc-form',
   templateUrl: './project-desc-form.component.html',
   styleUrls: ['./project-desc-form.component.css'],
 })
-export class ProjectDescFormComponent implements OnInit {
+export class ProjectDescFormComponent implements OnInit, OnChanges {
   projectDesc: FormGroup;
   @Input('project') projectData: ProjectData;
-  @Input('docRef') docRef:AngularFirestoreDocument<firebase.firestore.DocumentData>;
+  @Input('docRef')
+  docRef: AngularFirestoreDocument<firebase.firestore.DocumentData>;
+  @Input('added') added = false;
+  @Output('newProject') newProject = new EventEmitter<ProjectData>();
+
+  projectForm: FormGroup;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.projectDesc = this.fb.group({
-      name: ['',Validators.required],
+      name: ['', Validators.required],
       desc: [''],
       startTime: [''],
       endTime: [''],
       lifecycleStage: [''],
       gitHub: [''],
-      site:['']
+      site: [''],
     });
 
-    if(this.projectData){
+    if (this.projectData) {
       this.projectDesc.setValue({
         name: this.projectData.name,
         desc: this.projectData.desc,
@@ -35,22 +47,46 @@ export class ProjectDescFormComponent implements OnInit {
         endTime: this.projectData.endTime,
         lifecycleStage: this.projectData.lifecycleStage,
         gitHub: this.projectData.gitHub,
-        site:this.projectData.startTime
+        site: this.projectData.startTime,
       });
-    }
-    else {
-
+    } else {
+      this.createNewProject();
+      this.projectDesc.valueChanges.subscribe((value) => {
+        this.projectForm.patchValue(value);
+        this.newProject.emit(this.projectForm.value);
+      });
     }
   }
 
+  ngOnChanges() {
+    if (this.added) {
+      this.projectDesc.reset();
+    }
+  }
+  createNewProject() {
+    this.projectForm = this.fb.group({
+      name: ['', [Validators.required]],
+      desc: [''],
+      startTime: [''],
+      endTime: [''],
+      lifecycleStage: [''],
+      gitHub: [''],
+      site: [''],
+      tools: this.fb.array([]),
+      todos: this.fb.array([]),
+      features: this.fb.array([]),
+      questions: this.fb.array([]),
+      workTimes: this.fb.array([]),
+    });
+  }
   saveProjectDesc() {
-     this.docRef.update({
+    this.docRef.update({
       name: this.projectDesc.value.name,
       desc: this.projectDesc.value.desc,
       startTime: this.projectDesc.value.startTime,
       endTime: this.projectDesc.value.endTime,
       lifecycleStage: this.projectDesc.value.lifecycleStage,
       gitHub: this.projectDesc.value.gitHub,
-     })
+    });
   }
 }
