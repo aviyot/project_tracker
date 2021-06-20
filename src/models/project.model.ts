22a9a0c1b13537201a5e3d82a589901009b3da74
aviyot <aviyot@gmail.com>
2021-06-20 16:05:39 +0300

    remove unnecessary

--- src/models/project.model.ts
+++ src/models/project.model.ts
@@ -1,7 +1,3 @@
-import { Tool } from './tool.model';
-import { Feature } from './feature.model';
-import {QuestionAnswer} from "./question-answer.model"
-import { LIFECYCLE_STAGE } from '../types/lifecycleStage.type';
 import { ProjectData } from './project-data.model';
 
 export interface Project extends ProjectData {
