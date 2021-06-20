63054c974ed002377d66a9fd16e227a0fd49162f
aviyot <aviyot@gmail.com>
2021-06-20 16:17:47 +0300

    add type to user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ _user:Observable<any> = new Observable();
   constructor(private auth: AngularFireAuth) {
    }
 
-   get user():Observable<any>{
+   get user():Observable<firebase.User>{
     return this.auth.user;
   }
 }
