e70bbf78dcc8718ecbd47af268fe2ed6e96adcd5
aviyot <aviyot@gmail.com>
2021-06-20 18:29:18 +0300

    fix : upadte  edit to false afte change project

--- src/app/pages/description/description.component.ts
+++ src/app/pages/description/description.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Description } from 'src/models/description';
 import { Project } from 'src/models/project.model';
 import { FormState } from 'src/models/ui/form-state';
@@ -8,7 +8,7 @@ import { FormState } from 'src/models/ui/form-state';
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.css'],
 })
-export class DescriptionComponent implements OnInit {
+export class DescriptionComponent implements OnInit ,OnChanges{
   @Input() selectedProject: Project;
   @Input() docRef;
   projectDescState: FormState;
@@ -18,7 +18,14 @@ export class DescriptionComponent implements OnInit {
   ngOnInit(): void {
     this.projectDescState = { add: false, edit: false, selectedIndex: null };
   }
-
+  ngOnChanges(change : SimpleChanges){
+    if(change.selectedProject.previousValue){
+      if(change.selectedProject.currentValue !== change.selectedProject.previousValue ) {
+         this.projectDescState.edit = false;
+      }
+    }
+  }
+    
   editProjectDesc() {
     this.projectDescState.edit = !this.projectDescState.edit;
   }
