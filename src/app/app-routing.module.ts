8ee0ab6d6747144cc4077e4ce4cf447e8b28dfb6
aviyot <aviyot@gmail.com>
2021-07-21 15:50:49 +0300

    change file/folder structure!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { NewProjectComponent } from './forms/new-project/new-project.component';
-import { ProjectComponent } from './pages/project/project.component';
+import { NewProjectComponent } from './components/new-project/new-project/new-project.component';
+import { ProjectComponent } from './components/projects-list/project/project.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
-  {path:'new-project/edit/:project_id',component:NewProjectComponent},
-  {path:'new-project',component:NewProjectComponent},
-  {path:'app-projects', component:ProjectComponent},
-  {path:'app-project/:project_id',component:ProjectComponent},
-  {path:'',pathMatch:"full",redirectTo:'app-projects'}
-
+  { path: 'new-project/edit/:project_id', component: NewProjectComponent },
+  { path: 'new-project', component: NewProjectComponent },
+  { path: 'app-projects', component: ProjectComponent },
+  { path: 'app-project/:project_id', component: ProjectComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'app-projects' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
-    anchorScrolling: 'enabled',
-    scrollPositionRestoration: 'enabled',
-    onSameUrlNavigation:'reload'
-    
- })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+      onSameUrlNavigation: 'reload',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,28 +10,27 @@ import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthModule } from './auth/auth.module';
 
-import { NewProjectComponent } from './forms/new-project/new-project.component';
+import { NewProjectComponent } from './components/new-project/new-project/new-project.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProjectComponent } from './pages/project/project.component';
-import { ProjectListComponent } from './pages/project-list/project-list.component';
+import { ProjectComponent } from './components/projects-list/project/project.component';
+import { ProjectListComponent } from './components/projects-list/project-list/project-list.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
-import { TodoFormComponent } from './forms/todo-form/todo-form.component';
-import { WorkTimeFormComponent } from './forms/work-time-form/work-time-form.component';
-import { ProjectDescFormComponent } from './forms/project-desc-form/project-desc-form.component';
-import { ToolFormComponent } from './forms/tool-form/tool-form.component';
-import { FeatureFormComponent } from './forms/feature-form/feature-form.component';
-import { QuestionAnswerFormComponent } from './forms/question-answer-form/question-answer-form.component';
-import { DescriptionComponent } from './pages/description/description.component';
-import { ViewDataComponent } from './pages/view-data/view-data.component';
-import { ToolsComponent } from './pages/tools/tools.component';
-import { TodosComponent } from './pages/todos/todos.component';
-import { WorkTimesComponent } from './pages/work-times/work-times.component';
-import { FeaturesComponent } from './pages/features/features.component';
-import { QuestionsComponent } from './pages/questions/questions.component';
+import { TodoFormComponent } from './components/todo/todo-form/todo-form.component';
+import { WorkTimeFormComponent } from './components/work-time/work-time-form/work-time-form.component';
+import { ProjectDescFormComponent } from './components/description/project-desc-form/project-desc-form.component';
+import { ToolFormComponent } from './components/tool/tool-form/tool-form.component';
+import { FeatureFormComponent } from './components/feature/feature-form/feature-form.component';
+import { QuestionAnswerFormComponent } from './components/question-answer/question-answer-form/question-answer-form.component';
+import { DescriptionComponent } from './components/description/description/description.component';
+import { ToolsComponent } from './components/tool/tools/tools.component';
+import { TodosComponent } from './components/todo/todos/todos.component';
+import { WorkTimesComponent } from './components/work-time/work-times/work-times.component';
+import { FeaturesComponent } from './components/feature/features/features.component';
+import { QuestionsComponent } from './components/question-answer/questions/questions.component';
 import { FormatPipe } from './pipes/format.pipe';
 import { DatePipe } from '@angular/common';
-import { ChallengeFormComponent } from './forms/challenge-form/challenge-form.component';
-import { ChallengesComponent } from './pages/challenges/challenges.component';
+import { ChallengeFormComponent } from './components/challenge/challenge-form/challenge-form.component';
+import { ChallengesComponent } from './components/challenge/challenges/challenges.component';
 
 @NgModule({
   declarations: [
@@ -46,7 +45,6 @@ import { ChallengesComponent } from './pages/challenges/challenges.component';
     FeatureFormComponent,
     QuestionAnswerFormComponent,
     DescriptionComponent,
-    ViewDataComponent,
     ToolsComponent,
     TodosComponent,
     WorkTimesComponent,
