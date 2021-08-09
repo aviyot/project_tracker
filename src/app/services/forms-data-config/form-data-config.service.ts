89f947c055bd50d4b34b33f4da9c2a9143904d0a
aviyot <aviyot@gmail.com>
2021-08-09 21:14:10 +0300

    attempt to conver to model of array--Canceled

--- src/app/services/forms-data-config/form-data-config.service.ts
+++ src/app/services/forms-data-config/form-data-config.service.ts
@@ -8,6 +8,114 @@ import { TodoService } from './todo/todo.service';
 import { ToolsService } from './tools/tools.service';
 import { WorkTimesService } from './work-times/work-times.service';
 
+/* type Types =
+  | 'text'
+  | 'textarea'
+  | 'date'
+  | 'select'
+  | 'checkbox'
+  | 'href'
+  | 'array';
+class SectionField {
+  constructor(
+    private field: string,
+    private value: any[],
+    private type: Types,
+    private label: string,
+    private option?: string[]
+  ) {}
+}
+
+export class ProjectSection {
+  constructor(
+    private type: 'array' | 'map' | 'field',
+    private sectionName: string,
+    private sectionFields: SectionField[]
+  ) {}
+}
+
+let todo = new ProjectSection('array', 'todos', [
+  new SectionField('title', [''], 'text', 'שם משימה'),
+  new SectionField('detail', [''], 'textarea', 'פרטים'),
+  new SectionField('startTime', [new Date()], 'date', 'זמן התחלה'),
+  new SectionField('endTime', [''], 'date', 'תאריך סיום'),
+  new SectionField('status', [''], 'select', 'סטטוס'),
+  new SectionField('gitCommit', [''], 'text', 'קומיט'),
+  new SectionField('makingDesc', [''], 'textarea', 'איך נעשה'),
+]);
+
+let challenge = new ProjectSection('array', 'challenges', [
+  new SectionField('title', [''], 'text', 'קושי'),
+  new SectionField('detail', [''], 'textarea', 'תיאור'),
+  new SectionField('solution', [''], 'textarea', 'פתרונות'),
+]);
+
+let feature = new ProjectSection('array', 'features', [
+  new SectionField('title', [''], 'text', 'פיצר'),
+  new SectionField('detail', [''], 'textarea', 'פרטים'),
+  new SectionField('startTime', [new Date()], 'date', 'זמן התחלה'),
+  new SectionField('endTime', [''], 'date', 'תאריך סיום'),
+  new SectionField('status', [''], 'select', 'סטטוס'),
+]);
+
+let howTodos = new ProjectSection('array', 'howTodos', [
+  new SectionField('title', [''], 'text', 'פיצר'),
+  new SectionField('detail', [''], 'textarea', 'פרטים'),
+  new SectionField('byOrder', [true], 'checkbox', 'לפי סדר'),
+  new SectionField(
+    'steps',
+    [
+      [
+        new ProjectSection('array', 'steps', [
+          new SectionField('title', [''], 'text', 'פיצר'),
+          new SectionField('detail', [''], 'textarea', 'פרטים'),
+        ]),
+      ],
+    ],
+    'array',
+    'איך לעשות'
+  ),
+]);
+
+let projectDesc = new ProjectSection('map', 'projectDesc', [
+  new SectionField('name', [''], 'text', 'פיצר'),
+  new SectionField('desc', [''], 'textarea', 'פרטים'),
+  new SectionField('startTime', [new Date()], 'date', 'זמן התחלה'),
+  new SectionField('endTime', [''], 'date', 'תאריך סיום'),
+  new SectionField('lifecycleStage', ['planning'], 'select', 'שלב', [
+    'planning',
+    'design',
+    'prototyping',
+    'development',
+    'testing',
+    'publishing',
+    'maintenance',
+  ]),
+  new SectionField(
+    'sites',
+    [
+      new ProjectSection('array', 'site', [
+        new SectionField('label', [''], 'text', 'שם'),
+        new SectionField('site', [''], 'href', 'קישור'),
+      ]),
+    ],
+    'array',
+    'אתרים'
+  ),
+]);
+
+let tool = new ProjectSection('array', 'tools', [
+  new SectionField('name', [''], 'text', ''),
+  new SectionField('desc', [''], 'textarea', 'תיאור'),
+  new SectionField('desc', [''], 'textarea', 'תיאור'),
+  new SectionField('desc', [''], 'textarea', 'תיאור'),
+  new SectionField('purpose', [null], 'select', 'מטרה', [
+    'frontend',
+    'Backend',
+    'style',
+  ]),
+]);
+ */
 @Injectable({
   providedIn: 'root',
 })
