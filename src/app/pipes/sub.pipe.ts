d2dca0d497e041348a2345f6cfb18fe9db04cee5
aviyot <aviyot@gmail.com>
2021-08-09 19:28:05 +0300

    fix sub filter -if value undfiend return ''

--- src/app/pipes/sub.pipe.ts
+++ src/app/pipes/sub.pipe.ts
@@ -5,9 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SubPipe implements PipeTransform {
   transform(value: string, split: string, pos?: number): string {
-    if (!pos) {
-      pos = value.split(split).length - 1;
+    if (value) {
+      if (!pos) {
+        pos = value.split(split).length - 1;
+      }
+      return value.split(split)[pos];
+    } else {
+      return '';
     }
-    return value.split(split)[pos];
   }
 }
